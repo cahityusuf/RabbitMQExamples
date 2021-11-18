64d24034aa1c566a41ea9ad761d5704699cf001b
Cahit KAFADAR <cahit.kafadar@ardgrup.com.tr>
2021-11-18 09:11:31 +0300

    rabbitmq ornekleri eklendi

---
+++ Publish-Subscribe/EmitLog/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using static System.Console;
+class EmitLog
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+            WriteLine("[x] Sent {0}", message);
+        }
+        WriteLine("Press [enter] to exit.");
+        ReadLine();
+    }
+    private static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "info:Test Mesajı");
+    }
+}
+
+
---
+++ Publish-Subscribe/ReceiveLogs/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using static System.Console;
+class ReceiveLogs
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange:"logs", type: ExchangeType.Fanout);
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+            WriteLine("[*] Waiting for logs.");
+
+            var cosumer = new EventingBasicConsumer(channel);
+            cosumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                WriteLine("[x] {0}", message);
+            };
+
+            channel.BasicConsume(queue: queueName,autoAck:true,consumer: cosumer);
+            WriteLine("Press [enter] to exit.");
+            ReadLine();
+        }
+    }
+}
